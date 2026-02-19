20a3780b1e1637a0d358784fd8f0627913ee8a7c
daemonnd <daemonnd@protonmail.com>
2026-02-19 20:29:08 +0100

    add ai-summarizer.sh for ai summarization

---
+++ ai-summarizer.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# strict mode
+set -Eeuo pipefail
+
+# trap errors
+trap 'echo "Error on line $LINENO: command \"$BASH_COMMAND\" exited with status $?" >&2' ERR
+
+function init {
+    log_dir="$1"
+    user="$2"
+    # cp patterns to fabric
+    cp -r /home/${user}/.config/system-security-upgrader/system_prompts/* "/home/${user}/.config/fabric/patterns/"
+}
+function check_args {
+    echo "${1:?ERROR: The log dir is required as first argument}"
+    echo "${2:?ERROR: The username of the user having the config of these scipt is required as second argument!}"
+}
+
+function filter {
+    local logfile="$1"
+    awk '
+        /Warning:|Suggestion:/ {
+        sub(/^.*Warning: /, "Warning: ")
+        sub(/^.*Suggestion: /, "Suggestion: ")
+        print
+    }
+    ' "$logfile" | awk '!seen[$0]++' # uniq the loglines
+
+}
+function run_ai {
+    # tool: $1
+    local tool="$1"
+
+    fabric -sp "system_security_upgrader_$1" -o "/home/${user}/Documents/${tool}.md"
+}
+function main {
+    check_args "$@"
+    init "$@"
+    run_ai "lynis"
+
+}
+
+# call main with all args, as given
+main "$@"
+
